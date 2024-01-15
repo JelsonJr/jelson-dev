c563949790cf89a9f14f1786f12da68770dec3a9
Jelson <jelson.rodrigues.junior@gmail.com>
2024-01-14 22:09:57 -0300

    inciando menu mobile

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,14 +4,14 @@ export const StyledHeader = styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 10px 20px;
-    background-color: #333;
-    color: #fff;
+    padding: 10px 50px;
+    background-color: transparent;
+    color: #f7f7f7;
     height: 130px;
 
     .hamburger-menu {
         position: relative;
-        display: block;
+        display: none;
         width: 50px;
         height: 7px;
         background-color: white;
@@ -50,20 +50,81 @@ export const StyledHeader = styled.header`
             }
         }
 
-        &:hover::before,
-        &:hover::after {
-            background-color: #ddd;
+        @media (max-width: 768px) {
+            display: block;
+        }
+
+        @media (max-width: 480px) {
+            display: block;
+            width: 30px;
+            height: 4px;
+
+            &::before,
+            &::after {
+                height: 4px;
+            }
+
+            &::before {
+                top: -12px;
+            }
+
+            &::after {
+                top: 12px;
+            }
+
+            &.open {
+                transform: rotate(45deg);
+
+                &::before {
+                    top: 0;
+                    transform: rotate(90deg);
+                }
+
+                &::after {
+                    top: 0;
+                    transform: rotate(180deg);
+                }
+            }
         }
     }
 `;
 
 export const Logo = styled.div`
+    cursor: pointer;
     svg {
-        width: 70px;
-        height: 70px;
+        width: 100px;
+        height: 100px;
 
         path {
             fill: #fff;
+        }
+    }
+
+    @media (max-width: 1250px) {
+        svg {
+            width: 80px;
+            height: 80px;
+        }
+    }
+
+    @media (max-width: 940px) {
+        svg {
+            width: 70px;
+            height: 70px;
+        }
+    }
+
+    @media (max-width: 768px) {
+        svg {
+            width: 60px;
+            height: 60px;
+        }
+    }
+
+    @media (max-width: 480px) {
+        svg {
+            width: 50px;
+            height: 50px;
         }
     }
 `;
@@ -74,6 +135,7 @@ export const NavList = styled.ul`
     align-items: center;
     gap: 100px;
     list-style: none;
+    transition: 0.5s;
 
     li {
         text-decoration: none;
@@ -85,14 +147,34 @@ export const NavList = styled.ul`
         &:hover {
             opacity: 0.5;
         }
+    }
 
-        @media (max-width: 1250px) {
+    @media (max-width: 1250px) {
+        gap: 75px;
+
+        li {
             font-size: 24px;
-            color: #454555;
+        }
+    }
+
+    @media (max-width: 940px) {
+        gap: 45px;
+
+        li {
+            font-size: 20px;
         }
     }
 
     @media (max-width: 768px) {
-        display: none;
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+        margin-top: 450px;
+        text-align: center;
+
+        li {
+            text-align: center;
+            font-size: 16px;
+        }
     }
 `;
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,10 +1,23 @@
+import styled from "styled-components";
 import Header from "../../components/Header";
+import BackgroundImage from "../../assets/images/background.jpg";
+
+const Container = styled.section`
+    background-image: url(${BackgroundImage});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    /* display: flex;
+    justify-content: center;
+    align-items: center; */
+    height: 100vh;
+`;
 
 const Home = () => {
     return (
-        <>
+        <Container>
             <Header />
-        </>
+        </Container>
     );
 };
 
