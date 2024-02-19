9a5a215ee78bf68b013fc1e2049174479b1f58b6
Jelson <jelson.rodrigues.junior@gmail.com>
2024-02-19 14:00:24 -0300

    separando main page do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import styled from "styled-components";
-import { LanguagesProvider } from "context/contextLanguage";
 import GlobalStyles from "components/GlobalStyles";
-import NavigationMenu from "components/NavigationMenu";
-import Home from "views/Home";
-import AboutMe from "views/AboutMe";
+import MainPage from "views";
 import ErrorPage from "views/ErrorPage";
-import MyProjects from "views/MyProjects";
-import Contact from "views/Contact";
-import Footer from "components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LanguagesProvider } from "context/contextLanguage";
 import { ToastContainer } from "react-toastify";
 import { register } from "swiper/element/bundle";
 import "swiper/css";
@@ -21,33 +14,13 @@ import "react-tooltip/dist/react-tooltip.css";
 
 register();
 
-const Container = styled.main`
-    opacity: ${props => (props.$isMenuOpen ? 0.6 : 1)};
-`;
-
 function App() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
     return (
         <LanguagesProvider>
             <BrowserRouter>
                 <GlobalStyles />
                 <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <>
-                                <NavigationMenu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
-                                <Container $isMenuOpen={isMenuOpen}>
-                                    <Home />
-                                    <AboutMe />
-                                    <MyProjects />
-                                    <Contact />
-                                    <Footer />
-                                </Container>
-                            </>
-                        }
-                    />
+                    <Route path="/" element={<MainPage />} />
                     <Route path="/*" element={<ErrorPage />} />
                 </Routes>
                 <ToastContainer
