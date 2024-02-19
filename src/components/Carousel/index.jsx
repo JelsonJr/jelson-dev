303be29ccece686b7d030649ff63e8a3a6d1321d
Jelson <jelson.rodrigues.junior@gmail.com>
2024-02-19 13:55:46 -0300

    ajuste final no componente de carrossel

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,18 +1,36 @@
 import PropTypes from "prop-types";
 import Item from "./Item";
-import { Container } from "./styles";
+import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useRef } from "react";
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 960px;
+`;
 
 const Carousel = ({ items }) => {
+    const audioBreathingRef = useRef();
+
+    const handleSlider = () => {
+        const randomValue = Math.random().toFixed(1);
+        const probabilityToPlayAudio = 0.4;
+
+        if (randomValue <= probabilityToPlayAudio) {
+            audioBreathingRef.current.play();
+        }
+    };
+
     return (
         <Container>
-            <Swiper slidesPerView={1} navigation pagination={{ clickable: true }}>
-                {items.map(({ image, alt, description, link, name }, index) => (
+            <Swiper onSlideChange={handleSlider} slidesPerView={1} navigation pagination={{ clickable: true }}>
+                {items.map(({ image, alt, description, linkGithub, name }, index) => (
                     <SwiperSlide key={index}>
-                        <Item alt={alt} image={image} description={description} link={link} name={name} />
+                        <Item alt={alt} image={image} description={description} link={linkGithub} name={name} />
                     </SwiperSlide>
                 ))}
             </Swiper>
+            <audio ref={audioBreathingRef} src="/assets/audio/darthVaderBreathing.mp3" />
         </Container>
     );
 };
